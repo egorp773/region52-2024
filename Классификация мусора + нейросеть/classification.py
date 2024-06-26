41425e00e8b9b92e7e7663b6d85d9f83aaaf9fc0
egorp773 <episarenko773@gmail.com>
2024-06-26 13:10:19 +0300

    Classification.py

--- Классификация мусора + нейросеть/classification.py
+++ Классификация мусора + нейросеть/classification.py
@@ -5,61 +5,49 @@ import numpy as np
 import time
 import serial
 
-# Load the model and labels
-model = load_model("keras_Model.h5", compile=False)
-class_names = open("labels.txt", "r").readlines()
-url = 'http://172.20.10.11/'
-# Настройка порта Arduino
-ser = serial.Serial('COM3', 9600)
-bbcd = 10
-# Open the webcam
-camera = cv2.VideoCapture(url)
 
-time.sleep(15)
+model = load_model("keras_Model.h5", compile=False)  # Загружаем модель
+class_names = open("labels.txt", "r").readlines()  # Загружаем список классов
+
+# Настройка порта Arduino
+ser = serial.Serial('COM3', 9600)  # Создаем объект для связи с Arduino
+
+# Открытие веб-камеры
+camera = cv2.VideoCapture('http://172.20.10.11/')  # Открываем веб-камеру
+
+time.sleep(15)  # Ждем 15 секунд перед запуском
 while True:
-    # Capture image from webcam
-    ret, image = camera.read()
-    if not ret:  # Check if image capture was successful
-        print("Error: Could not capture image from webcam.")
+    ret, image = camera.read()  # Считываем кадр с камеры
+    if not ret:  # Проверяем, удалось ли получить кадр
+        print("Ошибка: Не удалось получить изображение.")
         break
 
-    # Resize image
-    image = cv2.resize(image, (224, 224))
+    image = cv2.resize(image, (224, 224))  # Изменяем размер изображения
+    image = np.asarray(image, dtype=np.float32) / 127.5 - 1 
+    image = image.reshape(1, 224, 224, 3)  \
 
-    # Preprocess image
-    image = np.asarray(image, dtype=np.float32) / 127.5 - 1
-    image = image.reshape(1, 224, 224, 3)
     time.sleep(2)
-    # Predict class
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    predicted_class = class_names[index][2:]
-    confidence_score = np.round(prediction[0][index] * 100, 2)
+    prediction = model.predict(image)  # Делаем предсказание
+    index = np.argmax(prediction)  # Находим индекс класса
+    predicted_class = class_names[index][2:]  # Получаем название класса
+    confidence_score = np.round(prediction[0][index] * 100, 2)  # Рассчитываем уверенность
 
-    if confidence_score > 90:
-        print(f"Class: {predicted_class}, Confidence: {confidence_score}%")
-        # # Send the command once before the if-else
+    if confidence_score > 90:  # Если уверенность больше 90%
+        print(f"Класс: {predicted_class}, Уверенность: {confidence_score}%")
+        if predicted_class == 'plastic\n':  
+            ser.write(f"{15}\n".encode())  # Отправляем команду 15 на Arduino
+        else:  # Если класс не "paper"
+            ser.write(f"{10}\n".encode())  # Отправляем команду 10 на Arduino
+        time.sleep(2)  # Ждем 2 секунды
 
-        if predicted_class == 'paper\n':  # Отправка 10 для пластика
-            print(11111)
-            ser.write(f"{15}\n".encode())
-            print(33333) 
-        else:
-            print(22222)
-            ser.write(f"{10}\n".encode())
-        time.sleep(2)
-
-    # Обработка для отображения изображения:
-    image = (image + 1) * 127.5
+    image = (image + 1) * 127.5  # Обрабатываем изображение для отображения
     image = image.astype(np.uint8)
     image = image[0]
 
-    # Display image
-    cv2.imshow("Webcam Image", image)
-    if cv2.waitKey(1) == 27:
+    cv2.imshow("Webcam Image", image)  # Отображаем изображение
+    if cv2.waitKey(1) == 27:  # Если нажата "Esc"
         break
 
-    # Exit on 'Esc' key
-camera.release()
-cv2.destroyAllWindows()
-ser.close()
+camera.release()  # Закрываем камеру
+cv2.destroyAllWindows()  # Закрываем окна
+ser.close()  # Закрываем подключение к Arduino
